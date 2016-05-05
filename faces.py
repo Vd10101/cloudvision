6dc350c0af4b0b2cf9f1e2a0372765886416d49c
source_server <source_server@example.com>
2016-05-05 18:59:31 +0000

    modify workspace

--- faces.py
+++ faces.py
@@ -55,7 +55,10 @@ def detect_face(face_file, max_results=4):
             'type': 'FACE_DETECTION',
             'maxResults': max_results,
             }]
-      
+      "imageContext":
+{
+
+}
         }]
 
     service = get_vision_service()
